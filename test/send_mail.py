8cc360139e45e1439197b71311cb3b68a85d0856
huxin <1362175426@qq.com>
2022-03-21 20:08:22 +0800

    desensitization

--- test/send_mail.py
+++ test/send_mail.py
@@ -5,10 +5,10 @@
 import yagmail
 
 # 链接邮箱服务器
-yag = yagmail.SMTP(user="h18279164409@163.com", password="VAGSZVJLVAYGLXCP", host='smtp.163.com')
+yag = yagmail.SMTP(user="xxx@xx.com", password="xxx", host='smtp.163.com')
 
 # 邮箱正文
 contents = ['This is the body, and here is just text']
 
 # 发送邮件
-yag.send('1362175426@qq.com', 'subject', contents)
+yag.send('xxx@xx.com', 'subject', contents)
